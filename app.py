e9e08d070a0086892796fa64d639f08005b18ef6
unniisme <122001019@smail.iitpkd.ac.in>
2024-01-20 12:23:47 +0530

    Updated website design

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from model.experiment_people import Model
 import json
 import os
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 # Initialization
 basepath = "data/"
 models = {}
@@ -108,14 +109,26 @@ def index(model_name):
         num_elements = int(request.form.get("num_elements"))
 
         precomputed = bool(request.form.get("precomputed", False))
+
+        alpha = float(request.form.get("alpha"))
+        beta = float(request.form.get("beta"))
+        gamma = float(request.form.get("gamma"))
             
 
-        res = model.calculate(modelpath, selected_entry, num_categories, num_elements, use_precalculated=precomputed)
+        try:
+            res = model.calculate(modelpath, selected_entry, num_categories, num_elements, 
+                            alpha=alpha, beta=beta, gamma=gamma, 
+                            use_precalculated=precomputed)
+            return render_template("index.html", 
+                                entries=entries, result=res, selected_entry=selected_entry, 
+                                num_categories=num_categories, num_elements=num_elements,
+                                precomputed_present=precomputed_present)
         
-        return render_template("index.html", 
-                               entries=entries, result=res, selected_entry=selected_entry, 
-                               num_categories=num_categories, num_elements=num_elements,
-                               precomputed_present=precomputed_present)
+        except KeyError:
+            flash("Unable to form complete groups with the given parameters")
+        except Exception as e:
+            flash("Unknown error " + str(e))
+        
 
     return render_template("index.html", entries=entries, result=None, selected_entry=None, 
                            num_categories=None, num_elements=None, precomputed_present=precomputed_present)
--- model/__init__.py
+++ model/__init__.py
@@ -1,3 +1,5 @@
 import logging
+from time import time
+logfile = f"log/model.{time()}.log"
 FORMAT = "[%(levelname)-8s][%(asctime)s][%(filename)s:%(lineno)s - %(funcName)13s() ] %(message)s"
-logging.basicConfig(format=FORMAT, filename='log/model.log', encoding='utf-8', level=logging.DEBUG)
+logging.basicConfig(format=FORMAT, filename=logfile, encoding='utf-8', level=logging.DEBUG)
